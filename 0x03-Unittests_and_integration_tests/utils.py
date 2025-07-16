b23e1949dd9214359890ce3039cc4a257820f7b2
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-07-17 00:49:25 +0200

    Cleared the comments in the code.

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -19,19 +19,6 @@ __all__ = [
 
 
 def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
-    """Access nested map with key path.
-    Parameters
-    ----------
-    nested_map: Mapping
-        A nested map
-    path: Sequence
-        a sequence of key representing a path to the value
-    Example
-    -------
-    >>> nested_map = {"a": {"b": {"c": 1}}}
-    >>> access_nested_map(nested_map, ["a", "b", "c"])
-    1
-    """
     for key in path:
         if not isinstance(nested_map, Mapping):
             raise KeyError(key)
@@ -41,28 +28,11 @@ def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
 
 
 def get_json(url: str) -> Dict:
-    """Get JSON from remote URL.
-    """
     response = requests.get(url)
     return response.json()
 
 
 def memoize(fn: Callable) -> Callable:
-    """Decorator to memoize a method.
-    Example
-    -------
-    class MyClass:
-        @memoize
-        def a_method(self):
-            print("a_method called")
-            return 42
-    >>> my_object = MyClass()
-    >>> my_object.a_method
-    a_method called
-    42
-    >>> my_object.a_method
-    42
-    """
     attr_name = "_{}".format(fn.__name__)
 
     @wraps(fn)
