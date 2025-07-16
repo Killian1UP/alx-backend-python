87279f4db7d240ae0583d94fc05f5ad8cbf77c37
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-07-16 22:23:50 +0200

    Updated the file to include mocking http calls.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
-# class TestAccessNestedMap(unittest.TestCase):
-#     @parameterized.expand([
-#         ({"a": 1}, ("a",), 1),
-#         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-#         ({"a": {"b": 2}}, ("a", "b"), 2)
-#     ])
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
     
     
-#     def test_access_nested_map(self, nested_map, path, expected):
-#         self.assertEqual(access_nested_map(nested_map, path), expected)
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
@@ -25,3 +26,19 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError)as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(str(cm.exception), f"'{expected_key}'")
+        
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        (("http://example.com"), {"payload": True}),
+        (("http://holberton.io"), {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        
+        result = get_json(test_url)
+        
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
