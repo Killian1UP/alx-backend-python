9f9653be48430f2e1c7e3aaf52634c0deb75899c
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-07-17 20:46:24 +0200

    Fixed a bug in the code.

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -60,3 +60,13 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(result, expected)
             mock_get_json.assert_called_once_with("http://awge.url")
             mock_url.assert_called_once()
+            
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test that has_license returns the correct boolean."""
+        client = GithubOrgClient("google")
+        result = client.has_license(repo, license_key)
+        self.assertEqual(result, expected)
