281f816d7cb7f9f0b2bd7a622d7d2e1604fe0560
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-07-19 03:19:04 +0200

    Viewsets for the chats app.

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Message, Conversation
+from .serializers import MessageSerializer, ConversationSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
 
 # Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
