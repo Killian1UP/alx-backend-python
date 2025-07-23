d7d5b896b1257198857db92a7d676d491141b6b2
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-07-23 20:47:08 +0200

    Modified the settings.

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'chats.apps.ChatsConfig',
     'rest_framework.authtoken',
+    'django_filter',
 ]
 
 MIDDLEWARE = [
@@ -153,5 +154,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 }
