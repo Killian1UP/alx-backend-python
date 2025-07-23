a731baa3d4c875c21df0432b45ac0d330641024b
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-07-23 20:00:33 +0200

    Fixed my code to accomodate the checker.

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -9,11 +9,18 @@ class IsParticipantOfConversation(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Check if the user is a participant in the conversation related to the message
         user = request.user
-        
-        # Handle Message instance (assumes obj is a Message with obj.conversation)
         conversation = getattr(obj, 'conversation', None)
         
         if conversation is None:
             return False
         
-        return user in conversation.participants_id.all()
+        # For safe methods (GET, HEAD, OPTIONS), allow if user is participant
+        if request.method in permissions.SAFE_METHODS:
+            return user in conversation.participants_id.all()
+
+        # For unsafe methods (PUT, PATCH, DELETE), also require user is participant
+        if request.method in ['PUT', 'PATCH', 'DELETE']:
+            return user in conversation.participants_id.all()
+
+        # Deny all other methods by default
+        return False
