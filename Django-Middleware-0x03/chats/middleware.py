1ad0b6e23746b6f98c76697119e95911292243ef
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-07-27 14:12:47 +0200

    Added a new middlware that restricts access between a period of time.

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from django.http import HttpResponseForbidden
 
 logging.basicConfig(
     filename='requests.log',
@@ -17,5 +18,17 @@ class RequestLoggingMiddleware:
         
         log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
         logging.info(log_message)
+        
+        return self.get_response(request)
+    
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        
+    def __call__(self, request):
+        current_hour = datetime.now().hour
+        
+        if current_hour < 18 or current_hour >= 21:
+            return HttpResponseForbidden("Access is only allowed between 6pm or 9pm.")
         
         return self.get_response(request)
