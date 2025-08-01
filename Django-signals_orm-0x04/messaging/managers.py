844b9edc41d7f58a4f9e7ec9bfc9a3092e65dd29
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-08-01 20:23:30 +0200

    File for the custom manager.

---
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class UnreadMessagesManager(models.Manager):
+    def for_user(self, user):
+        return self.filter(receiver=user, read=False)
+
+    def unread_count(self, user):
+        return self.for_user(user).count()
